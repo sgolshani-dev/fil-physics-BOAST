2f78048310611bd103aa62ebc0a7ea99a11d6b1b
sgolshn <s.golshani@ucl.ac.uk>
2025-04-03 19:03:51 +0100

    Update SetDefaultScannerParam.m

--- SetDefaultScannerParam.m
+++ SetDefaultScannerParam.m
@@ -1,11 +1,22 @@
 function scanner_param = SetDefaultScannerParam
+% This is no longer needed!
+% =========================================================================
+% This function sets the scanner parameters. 
+% These values can be modified as needed to suit specific requirements.
+% =========================================================================
+% name                            : Scanner name
+% B0                              : Field strength
+% R2s                             : R2* value (in s^-1) 
+%                                 - An averaged value over the entire brain.
+%                                   Although not directly a scanner parameter, 
+%                                   it depends on the field strength!
+% =========================================================================
 
-our_scanner.name = 'Trio';
-our_scanner.B0 = 3;
-% well this is not directly a scanner parameter, however it depends on the
-% field strength and therefore scanner
-our_scanner.T2s = 45*10^-3; 	% T2* at 3 T (Wansapura et al., JMRI 1999)
+% Updated 23/09/2024
+% by Shokoufeh Golshani
 
-scanner_param = our_scanner;
+scanner_param.name = 'Trio';
+scanner_param.B0 = 3;
+scanner_param.R2s = 1/45e-3; 	    % T2* at 3 T = 45 ms (Wansapura et al., JMRI 1999)
 
 end
